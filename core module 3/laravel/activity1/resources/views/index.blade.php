3b4d7b5bae4d1b777cb801d1107ddce41118cf48
Nafih zain <stupidzerox@gmail.com>
2024-02-12 16:47:27 +0530

    Initial commit

---
+++ core module 3/laravel/activity1/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <title>
+            laravel
+        </title>
+        <body>
+            <h1>This is first program</h1>
+        </body>
+    </head>
